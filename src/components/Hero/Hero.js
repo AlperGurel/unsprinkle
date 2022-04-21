2b0714359f85e981717fd048cef1d86a9952fb82
Alper Gurel <alper.gurel@revolvia.com>
2022-04-21 10:30:25 +0300

    exercise

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
+      <picture>
+        <source
+          type="image/avif"
+          srcSet="
+           /images/hero-img.avif 1x,
+           /images/hero-img@2x.avif 2x,
+           /images/hero-img@3x.avif 3x
+          "
+        />
+        <source
+          type="image/jpg"
+          srcSet="
+           /images/hero-img.jpg 1x,
+           /images/hero-img@2x.jpg 2x,
+           /images/hero-img@3x.jpg 3x
+          "
+        />
+        <HeroImage
+          src="/images/hero-img.jpg"
+          alt="Hero image with a cute cat."
+        />
+      </picture>
       <Swoop src="/swoop.svg" />
     </Wrapper>
   );
@@ -25,6 +46,7 @@ const HeroImage = styled.img`
   width: 500px;
   height: 500px;
   max-height: 100%;
+  object-fit: cover;
 `;
 
 const Swoop = styled.img`
--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
+  const avifSrc = src.replace(".jpg", ".avif");
+  const avifSrc2x = src.replace(".jpg", "@2x.avif");
+  const avifSrc3x = src.replace(".jpg", "@3x.avif");
+
+  const jpgSrc = src.replace(".jpg", ".jpg");
+  const jpgSrc2x = src.replace(".jpg", "@2x.jpg");
+  const jpgSrc3x = src.replace(".jpg", "@3x.jpg");
+
+  const labels = tags.toString();
+
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source
+            type="image/avif"
+            srcSet={`${avifSrc} 1, ${avifSrc2x} 2, ${avifSrc3x} 3`}
+          />
+          <source
+            type="image/jpg"
+            srcSet={`${jpgSrc} 1, ${jpgSrc2x} 2, ${jpgSrc3x} 3`}
+          />
+          <Image src={src} alt={`Photo in grid with tags: ${labels}`} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,15 +48,20 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  padding-top: 8px;
+  padding-bottom: 8px;
 `;
 
 const Tag = styled.li`
+  display: inline;
+  margin-right: 4px;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
